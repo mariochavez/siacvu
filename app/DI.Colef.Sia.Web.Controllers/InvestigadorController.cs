1c8d24d1366e7c33293e5173c1ddc0be886b4709
Giovani <giovanirguz@gmail.com>
2009-07-28 14:31:54 -0700

    Implementacion del controlador para Investigador con los mappers correspondientes.

---
+++ app/DI.Colef.Sia.Web.Controllers/InvestigadorController.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Web.Mvc;
+using DecisionesInteligentes.Colef.Sia.ApplicationServices;
+using DecisionesInteligentes.Colef.Sia.Core;
+using DecisionesInteligentes.Colef.Sia.Web.Controllers.Mappers;
+using DecisionesInteligentes.Colef.Sia.Web.Controllers.Models;
+using SharpArch.Web.NHibernate;
+
+namespace DecisionesInteligentes.Colef.Sia.Web.Controllers
+{
+    [HandleError]
+    public class InvestigadorController : BaseController<Investigador, InvestigadorForm>
+    {
+		readonly IInvestigadorService investigadorService;
+        readonly IInvestigadorMapper investigadorMapper;
+    
+        public InvestigadorController(IInvestigadorService investigadorService, IInvestigadorMapper investigadorMapper) 
+        {
+            this.investigadorService = investigadorService;
+            this.investigadorMapper = investigadorMapper;
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Index() 
+        {
+			var data = CreateViewDataWithTitle(Title.Index);
+			
+			var investigadors = investigadorService.GetAllInvestigadors();
+            data.List = investigadorMapper.Map(investigadors);
+
+            return View(data);
+        }
+        
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult New()
+        {			
+			var data = CreateViewDataWithTitle(Title.New);
+            data.Form = new InvestigadorForm();
+			
+			return View(data);
+        }
+        
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Edit(int id)
+        {
+            var data = CreateViewDataWithTitle(Title.Edit);
+
+            var investigador = investigadorService.GetInvestigadorById(id);
+            data.Form = investigadorMapper.Map(investigador);
+
+			ViewData.Model = data;
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Show(int id)
+        {
+            var data = CreateViewDataWithTitle(Title.Show);
+
+            var investigador = investigadorService.GetInvestigadorById(id);
+            data.Form = investigadorMapper.Map(investigador);
+            
+            ViewData.Model = data;
+            return View();
+        }
+        
+        [Transaction]
+        [ValidateAntiForgeryToken]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(InvestigadorForm form)
+        {
+        
+            var investigador = investigadorMapper.Map(form);
+
+            if(!IsValidateModel(investigador, form, Title.New))
+                return ViewNew();
+
+            investigadorService.SaveInvestigador(investigador);
+
+            return RedirectToIndex(String.Format("{0} ha sido creado", investigador.Nombre));
+        }
+        
+        [Transaction]
+        [ValidateAntiForgeryToken]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Update(InvestigadorForm form)
+        {
+        
+            var investigador = investigadorMapper.Map(form);
+
+            if (!IsValidateModel(investigador, form, Title.Edit))
+                return ViewEdit();
+
+            investigadorService.SaveInvestigador(investigador);
+
+            return RedirectToIndex(String.Format("{0} ha sido modificado", investigador.Nombre));
+        }
+        
+        [Transaction]
+        [AcceptVerbs(HttpVerbs.Put)]
+        public ActionResult Activate(int id)
+        {
+            var investigador = investigadorService.GetInvestigadorById(id);
+            investigador.Activo = true;
+            investigadorService.SaveInvestigador(investigador);
+
+            var form = investigadorMapper.Map(investigador);
+            
+            return Rjs(form);
+        }
+        
+        [Transaction]
+        [AcceptVerbs(HttpVerbs.Put)]
+        public ActionResult Deactivate(int id)
+        {
+            var investigador = investigadorService.GetInvestigadorById(id);
+            investigador.Activo = false;
+            investigadorService.SaveInvestigador(investigador);
+
+            var form = investigadorMapper.Map(investigador);
+            
+            return Rjs("Activate", form);
+        }
+    }
+}
