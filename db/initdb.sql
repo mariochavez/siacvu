adc71be13b1f51232447bc3e6047a4de02e80ebc
Alex Osuna <alejandro.osuna87@gmail.com>
2009-07-28 11:33:39 -0700

    Scritp de SQL llamado initdb.sql que permite cargar los registros de cada catalogo

---
+++ db/initdb.sql
@@ -0,0 +1,59 @@
+insert into Estado values(1,'Activo');
+insert into Estado values(1,'Baja');
+insert into Estado values(1,'Sabático');
+insert into Estado values(1,'Licencia c/ sueldo');
+insert into Estado values(1,'Licencia s/ sueldo');
+insert into Estado values(1,'Superación académica');
+
+insert into GradoAcademicos values(1,'Licenciatura');
+insert into GradoAcademicos values(1,'Maestría');
+insert into GradoAcademicos values(1,'Doctor');
+
+insert into Categorias values(1,'Titular A');
+insert into Categorias values(1,'Titular B');
+insert into Categorias values(1,'Titular C');
+insert into Categorias values(1,'Asociado A');
+insert into Categorias values(1,'Asociado B');
+insert into Categorias values(1,'Asociado C');
+
+insert into Cargos values(1,'Investigador');
+insert into Cargos values(1,'Director de Departamento');
+insert into Cargos values(1,'Director Regional');
+insert into Cargos values(1,'Coordinador de Programa');
+insert into Cargos values(1,'Presidente');
+insert into Cargos values(1,'Secretario General Académico');
+insert into Cargos values(1,'Coordinador de Departamento de Apoyo');
+insert into Cargos values(1,'Coordinador de Oficinas de Enlace y Vinculación');
+insert into Cargos values(1,'Director de Posgrado');
+insert into Cargos values(1,'Director General de Vinculación y Apoyo Académico');
+insert into Cargos values(1,'Director General Académico');
+insert into Cargos values(1,'Becario de Alto Nivel');
+insert into Cargos values(1,'Investigador por Proyecto');
+insert into Cargos values(1,'Secretario General de Planeación y Desarrollo Institucional');
+insert into Cargos values(1,'Direccion General de Administracion y Finanzas');
+insert into Cargos values(1,'Direccion General Regional Noroeste');
+insert into Cargos values(1,'Direccion de Area');
+insert into Cargos values(1,'Otros');
+insert into Cargos values(1,'Coordinador de Posgrado');
+insert into Cargos values(1,'Coordinador de EcoParque');
+insert into Cargos values(1,'Asistente de Director');
+insert into Cargos values(1,'Estudiante Becario');
+insert into Cargos values(1,'Investigador visitante');
+insert into Cargos values(1,'Director General de Docencia');
+insert into Cargos values(1,'Director General de Vinculación Institucional');
+insert into Cargos values(1,'Director General de Asuntos Académicos');
+
+insert into SNIs values(1,'Candidato');
+insert into SNIs values(1,'Nivel 1');
+insert into SNIs values(1,'Nivel 2');
+insert into SNIs values(1,'Nivel 3');
+insert into SNIs values(1,'Emerito');
+
+insert into Sedes values(1,'Matamoros','Dirección General Regional Noreste');
+insert into Sedes values(1,'Nuevo Laredo','Dirección General Regional Noreste');
+insert into Sedes values(1,'Piedras Negras','Dirección General Regional Noreste');
+insert into Sedes values(1,'Monterrey','Dirección General Regional Noreste');
+insert into Sedes values(1,'Ciudad Juarez','Dirección General Regional Noroeste');
+insert into Sedes values(1,'Tijuana','Tijuana');
+insert into Sedes values(1,'Mexicali','Dirección General Regional Noroeste');
+insert into Sedes values(1,'Nogales','Dirección General Regional Noroeste');
