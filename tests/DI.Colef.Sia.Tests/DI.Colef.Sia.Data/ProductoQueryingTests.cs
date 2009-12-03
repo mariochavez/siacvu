83373c9ec7114accfb4e35908487685624b30c31
Mario A Chavez <mario.chavez@gmail.com>
2009-12-03 13:09:08 -0800

    Archivos faltantes para proceso de bandeja

---
+++ tests/DI.Colef.Sia.Tests/DI.Colef.Sia.Data/ProductoQueryingTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using DecisionesInteligentes.Colef.Sia.Core;
+using DecisionesInteligentes.Colef.Sia.Core.DataInterfaces;
+using Machine.Specifications;
+using Rhino.Mocks;
+using Tests.Extensions;
+
+namespace Tests.DI.Colef.Sia.Data
+{
+    [Subject(typeof(ProductoQuerying))]
+    public class when_bandeja_investigador_is_loaded : ConnectionSetup
+    {
+        static IProductoQuerying productoQuerying;
+        static ProductoDTO[] productosDto;
+
+        Because of = () =>
+            {
+                Usuario usuario = MockRepository.GenerateStub<Usuario>();
+                Type userType = typeof(Usuario);
+                PropertyInfo pi = userType.GetProperty("Id");
+                pi.SetValue(usuario, 3, null);
+
+                productoQuerying = new ProductoQuerying();
+                productosDto = productoQuerying.GetProductosBandeja(usuario, DateTime.Now);
+            };
+
+        It should_found_3_products = () =>
+            {
+                productosDto.ShouldNotBeEmpty();
+                productosDto.Length.ShouldEqual(3);
+            };
+    }
+}
