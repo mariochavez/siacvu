a5beab9a9e2ec615020c30eb7f5958e7e933de3b
Mario A Chavez <mario.chavez@gmail.com>
2009-12-03 09:34:43 -0800

    Nueva convencion para nhibernate, para guardar archivos en la db

---
+++ app/DI.Colef.Sia.Data/NHibernateMaps/Conventions/ByteArrayConvention.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Mapping;
+
+namespace DecisionesInteligentes.Colef.Sia.Data.NHibernateMaps.Conventions
+{
+    public class ByteArrayConvention : IPropertyConvention
+    {
+        public bool Accept(IProperty property)
+        {
+            return true;
+        }
+
+        public void Apply(IProperty property)
+        {
+            if(property.PropertyType == typeof(byte[]))
+                property.CustomSqlTypeIs("image");
+        }
+    }
+}
