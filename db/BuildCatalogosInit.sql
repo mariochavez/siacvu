722dce8ad15e10c6902d6728e0a0fb82a08e3933
Mario Alberto Chavez <mario.chavez@gmail.com>
2009-12-28 10:47:45 -0800

    Nuevo archivo para inicializar catalogos, de uso del colef unicamente. Modificacion al script de build para que incluya el nuevo initcatalogos.sql

---
+++ db/BuildCatalogosInit.sql
@@ -0,0 +1,70 @@
+use siacvu
+
+exec sp_generate_inserts 'sectores'
+exec sp_generate_inserts 'ambitos'
+exec sp_generate_inserts 'paises'
+exec sp_generate_inserts 'estadopaises'
+
+exec sp_generate_inserts 'AreaInvestigaciones'
+exec sp_generate_inserts 'Indices'
+exec sp_generate_inserts 'instituciones'
+
+exec sp_generate_inserts 'editoriales'
+exec sp_generate_inserts 'revistapublicaciones'
+exec sp_generate_inserts 'editoriales'
+
+exec sp_generate_inserts 'roles'
+exec sp_generate_inserts 'usuariorol'
+exec sp_generate_inserts 'consejocomisiones'
+
+exec sp_generate_inserts 'dirigidoas'
+exec sp_generate_inserts 'estatusformacionacademicas'
+exec sp_generate_inserts 'monedas'
+
+exec sp_generate_inserts 'tipoestancias'
+exec sp_generate_inserts 'tipoproyectos'
+exec sp_generate_inserts 'medioimpresos'
+exec sp_generate_inserts 'medioelectronicos'
+
+
+exec sp_generate_inserts 'tipoorganos'
+exec sp_generate_inserts 'tipodictamenes'
+exec sp_generate_inserts 'tipodistinciones'
+
+
+exec sp_generate_inserts 'tipoeventos'
+exec sp_generate_inserts 'departamentos'
+exec sp_generate_inserts 'estados'
+
+exec sp_generate_inserts 'gradoacademicos'
+exec sp_generate_inserts 'categorias'
+exec sp_generate_inserts 'puestos'
+exec sp_generate_inserts 'snis'
+
+exec sp_generate_inserts 'tipoparticipaciones'
+exec sp_generate_inserts 'tipoactividades'
+exec sp_generate_inserts 'idiomas'
+exec sp_generate_inserts 'lineainvestigaciones'
+
+
+exec sp_generate_inserts 'lineatematicas'
+exec sp_generate_inserts 'areatematicas'
+exec sp_generate_inserts 'programaestudios'
+exec sp_generate_inserts 'nivelestudios'
+
+exec sp_generate_inserts 'tipoapoyos'
+exec sp_generate_inserts 'subprogramaconacyts'
+exec sp_generate_inserts 'fondoconacyts'
+exec sp_generate_inserts 'tipoarchivos'
+exec sp_generate_inserts 'vinculacionapydes'
+exec sp_generate_inserts 'direccionregionales'
+exec sp_generate_inserts 'sedes'
+exec sp_generate_inserts 'tipoarchivos'
+
+exec sp_generate_inserts 'sectores'
+exec sp_generate_inserts 'organizaciones'
+exec sp_generate_inserts 'niveles'
+exec sp_generate_inserts 'disciplinas'
+exec sp_generate_inserts 'subdisciplinas'
+exec sp_generate_inserts 'ramas'
+exec sp_generate_inserts 'clases'
