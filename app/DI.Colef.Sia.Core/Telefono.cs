aa5316d2b6c55cf775e675459c732fd3e4886b6e
Fernando Castellanos <fernandocastellanos@gmail.com>
2010-06-24 13:40:49 -0700

    Archivos nuevos para poder agregar Direccion, Telefono, CorreoElectronico a un Usuaio, del sistema

---
+++ app/DI.Colef.Sia.Core/Telefono.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Validator.Constraints;
+using SharpArch.Core.DomainModel;
+
+namespace DecisionesInteligentes.Colef.Sia.Core
+{
+    public class Telefono : Entity, IBaseEntity
+    {
+        [NotNullNotEmpty]
+        [Length(20)]
+        public virtual string Numero { get; set; }
+
+        public virtual int TipoTelefono { get; set; }
+
+        public virtual Usuario CreadoPor { get; set; }
+
+        public virtual DateTime CreadoEl { get; set; }
+
+        public virtual Usuario ModificadoPor { get; set; }
+
+        public virtual DateTime ModificadoEl { get; set; }
+
+        public virtual bool Activo { get; set; }
+
+        public virtual IList<Usuario> Usuarios { get; private set; }
+
+        public virtual void AddUsuario(Usuario usuario)
+        {
+            Usuarios.Add(usuario);
+        }
+    }
+}
---
+++ app/DI.Colef.Sia.Web/Views/Usuario/_NewTelefono.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" Inherits="System.Web.Mvc.ViewUserControl<UsuarioForm>" %>
+<%@ Import Namespace="DecisionesInteligentes.Colef.Sia.Web.Extensions"%>
+<%@ Import Namespace="DecisionesInteligentes.Colef.Sia.Web.Controllers.Models"%>
+
+<p>
+    <label>Tipo tel&eacute;fono</label>
+</p>
+<p>
+    <label>N&uacute;mero de tel&eacute;fono</label>
+</p>
